490e347b69645122e9aaaeaf5d89d6667f32b868
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-11-30 16:48:52 +0100

    install mongoDB on local

--- authentication/config.js
+++ authentication/config.js
@@ -4,5 +4,5 @@ dotenv.config()
 export default {
 	HOST : process.env.HOST || "127.0.0.1",
 	PORT : process.env.PORT || 8088,
-	MONGO_URI : process.env.MONGO_URI || "mongodb://localhost/node_test" || "mongodb:user:pass@//localhost/node_test",
+	MONGO_URI : process.env.MONGO_URI || "mongodb://localhost:27017/node_test" || "mongodb:user:pass@//localhost:27017/node_test",
 }
