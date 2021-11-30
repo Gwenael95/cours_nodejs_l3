dc5e5e511630e072ef61fa5a7fcbabdc0fc39ea9
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-11-30 16:40:28 +0100

    video S006 check data send + test

--- authentication/todos.controller.js
+++ authentication/todos.controller.js
@@ -4,6 +4,7 @@
  * @param res
  */
 import {createTodo} from "./todos.services.js";
+import {checkPostTodos} from "./todos.validator.js"
 
 export function getAllTodosController(req, res){
 
@@ -16,6 +17,12 @@ export function getOneTodosController(req, res){
 }
 
 export async function postTodosController(req, res){
+    const check = checkPostTodos(req.body)
+    if(check !== true){
+        return res.status(400).json({
+            error : check
+        }) // bad request
+    }
     const todo = await createTodo(req.body.name, req.body.items)
     res.json(todo)
 }
