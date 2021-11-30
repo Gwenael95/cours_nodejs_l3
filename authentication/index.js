9f085a14c3365f706ebb2456d0889eb5972341d5
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-11-30 14:42:11 +0100

    video S002 server

--- authentication/index.js
+++ authentication/index.js
@@ -1,9 +1,18 @@
 // Prepare to include the server code into our web_server
-import express from 'express' // without type:module => const express = require('express')
-import http from 'http'
+// install all necessary package with "npm install"
+// should add .env in .gitignore
 
-const PORT = 8080
-const HOST = "127.0.0.1"
+//classic start (prod) : npm start. 
+//dev start : npm run dev. it will restart server when updating a file 
+
+//@todo : create file .env . its content is 
+// PORT=8088
+// HOST=127.0.0.1 
+
+import express from "express" // without type:module => const express = require("express")
+import http from "http"
+import config from "./config.js"
+
 const app = express()
 const server = http.createServer(app)
 
@@ -15,28 +24,27 @@ app.use((req, res, next)=>{
 
 //parse req body as Object, available in req.body
 app.use(express.json())
-app.use(express.urlencode( { extended:true } ))
+app.use(express.urlencoded( { extended:true } ))
 
 
-
-server.listen(PORT, HOST, ()=>{
-	console.log("listening " + HOST )
+server.listen(config.PORT,config.HOST, ()=>{
+	console.log("listening on http://" + config.HOST + ":" + config.PORT )
 })
 
 
 
 
 /*
-import express from 'express' // without type:module => const express = require('express')
-const http = require('http')
-const cors = require('cors')
+import express from "express" // without type:module => const express = require("express")
+const http = require("http")
+const cors = require("cors")
 const app = express()
 const server = http.createServer(app)
 
-app.use(express.static('html'))
+app.use(express.static("html"))
 
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
@@ -47,19 +55,19 @@ const io = require('socket.io')(server, {
 
 const pseudos = []
 const clients = {
-    'client.id': 'pseudo'
+    "client.id": "pseudo"
 }
 
 let connected = 0
 
-io.on('connection', function(socket_client) {
-    console.log('Client connected', socket_client.id)
+io.on("connection", function(socket_client) {
+    console.log("Client connected", socket_client.id)
 })
 
 const port = process.env.PORT || 8080
 
 server.listen(port, () => {
-    console.log('started')
+    console.log("started")
 })
 
 
